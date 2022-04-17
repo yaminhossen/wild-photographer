c466f773b58b682c1e46089dadd89d8656370649
yaminhossen <streatdot616@gmail.com>
2022-04-17 20:32:22 +0600

    login and register compo added toggle

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import About from './Pages/About/About';
 import CheckOut from './Pages/CheckOut/CheckOut';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Notfound from './Pages/Shared/NotFound/Notfound';
@@ -16,6 +18,8 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/service/:serviceId' element={<CheckOut></CheckOut>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<Notfound></Notfound>}></Route>
       </Routes>
 
---
+++ src/Pages/Login/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>this is register</h3>
+        </div>
+    );
+};
+
+export default Register;
